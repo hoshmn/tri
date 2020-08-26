9ccd675564e2ad8ebab42f3395db2e66b414fe1b
Josh Maas-Howard <j.maas.howard@gmail.com>
2020-08-26 01:32:07 -0400

    cycling pants

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
+const pantsSources = [
+  { src: 'pants_0.jpg' },
+  { src: 'pants_1.png' },
+  { src: 'pants_2.png' },
+  { src: 'pants_3.mov', video: true },
+]
+const pantsFrameSize = { height: '453px', width: '604px' }
+let pantsSourceIndex = 0
+
+const showNextSource = (frame) => {
+  pantsSourceIndex = (pantsSourceIndex + 1) % pantsSources.length
+  const { src, video } = pantsSources[pantsSourceIndex]
+  const elem = video ? 'video autoplay loop muted preload="auto"' : 'img'
+  const childHtml = `<${elem} src="${src}"></${elem}>`
+  console.log('childhtml', childHtml)
+  frame.html(childHtml)
+}
+
+
+
+$('.frame.pants').click(function() {
+  showNextSource($(this))
+})
+$('.frame.pants').css(pantsFrameSize)
+
+showNextSource($('.frame.pants'))
+// $('img.trivg').mouseleave(function() {
+//   $(this).attr('src', 'trivg.png')
+// })
+
+
+
+
+
+
 const tris = $('.triangle')
 
-tris.hover(function(t) {
+tris.hover(function() {
   $(this).addClass('touched')
 })
