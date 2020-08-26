1e3f736a036a0cf89af5b80d1ad596bb97dca937
Josh Maas-Howard <j.maas.howard@gmail.com>
2020-08-26 11:56:33 -0400

    - add tree - streamline additions with dynamic init - polish gallery styles

--- index.js
+++ index.js
@@ -1,36 +1,91 @@
-const pantsSources = [
-  // { src: 'pants_0.jpg' },
-  { src: 'pants_1.png' },
-  { src: 'pants_2.png' },
-  { src: 'pants_3.mov', video: true },
-]
-const pantsFrameSize = { height: '453px', width: '604px' }
-let nextPantsIndex = 0
 
-const showNextSource = (frame) => {
-  const { src, video } = pantsSources[nextPantsIndex]
-  nextPantsIndex = (nextPantsIndex + 1) % pantsSources.length
-  const elem = video ? 'video autoplay loop muted preload="auto"' : 'img'
-  const childHtml = `<${elem} src="${src}"></${elem}>`
-  frame.html(childHtml)
+const DEFAULT_LANDSCAPE_WIDTH = 900
+const DEFAULT_PORTRAIT_WIDTH = 675
+
+// __CONTENT_____
+
+// to add a new "frame", simply define a frame obj like the following and add it to the frames array:
+const pants = {
+  id: 'pants',
+  sources: [ // as many as you'd like! just make sure to upload an image/gif file with a matching name
+    // { src: 'pants_0.jpg' },
+    { src: 'pants.gif' },
+    { src: 'pants_1.png' },
+    { src: 'pants_2.png' },
+  ],
+  frameSize: { 'max-width': DEFAULT_LANDSCAPE_WIDTH }
 }
 
+const tree = {
+  id: 'tree',
+  sources: [
+    // { src: 'tree_0.jpg' },
+    { src: 'tree.gif' },
+    { src: 'tree_1.png' },
+    { src: 'tree_2.png' },
+  ],
+  // frameSize: { height: 1311, width: 983 }
+  frameSize: { 'max-width': DEFAULT_PORTRAIT_WIDTH }
+}
+
+const frames = [ pants, tree ]
 
 
-$('.frame.pants').click(function() {
-  showNextSource($(this))
+// __INTERNAL LOGIC_____
+
+const frameMap = {} // populates dynamically, keep as empty obj
+
+const showNextSource = (frame) => {
+  const { id, sources } = frame
+  const { src, video } = getNextSource(frame)
+
+  const $frame = $(`#${id}.frame`)
+  $frame.find('img').attr('src', src)
+
+  // for video
+  // const elem = video ? 'video autoplay loop muted preload="auto"' : 'img'
+  // const childHtml = `<${elem} src="${src}"></${elem}>`
+  // frame.html(childHtml)
+}
+
+const getNextSource = frame => {
+  const { activeIdx, sources } = frame
+  if (!Number.isInteger(activeIdx)) {
+    frame.activeIdx = 0
+  } else {
+    frame.activeIdx = (activeIdx + 1) % sources.length
+  }
+
+  return sources[frame.activeIdx]
+}
+
+const initialize = () => {
+  frames.forEach((frame) => {
+    const { id, frameSize, sources } = frame
+    frameMap[id] = frame // populate map, to retrieve proper frame on click
+
+    const { src, video } = getNextSource(frame)
+    const frameDiv = `
+      <div data-id="${id}" id="${id}" class="frame">
+        <img src="${src}" alt="${id}">
+      </div>
+    `
+    
+    $('.frames').append(frameDiv)
+
+    const $frame = $(`#${id}.frame`)
+    $frame.css(frameSize)
+    // showNextSource(frame)
+  })
+}
+
+initialize()
+
+$('.frame').click(function() {
+  const { id } = this.dataset
+  const frame = frameMap[id]
+  showNextSource(frame)
 })
-$('.frame.pants').css(pantsFrameSize)
-
-showNextSource($('.frame.pants'))
-// $('img.trivg').mouseleave(function() {
-//   $(this).attr('src', 'trivg.png')
-// })
-
-
-
-
-
 
 const tris = $('.triangle')
 
