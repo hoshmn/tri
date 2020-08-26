7e85af9db8fb8be384dc55d7e73ce1612b38ef71
Josh Maas-Howard <j.maas.howard@gmail.com>
2020-08-26 01:56:35 -0400

    frame expand

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const pantsSources = [
-  { src: 'pants_0.jpg' },
+  // { src: 'pants_0.jpg' },
   { src: 'pants_1.png' },
   { src: 'pants_2.png' },
   { src: 'pants_3.mov', video: true },
 ]
 const pantsFrameSize = { height: '453px', width: '604px' }
-let pantsSourceIndex = 0
+let nextPantsIndex = 0
 
 const showNextSource = (frame) => {
-  pantsSourceIndex = (pantsSourceIndex + 1) % pantsSources.length
-  const { src, video } = pantsSources[pantsSourceIndex]
+  const { src, video } = pantsSources[nextPantsIndex]
+  nextPantsIndex = (nextPantsIndex + 1) % pantsSources.length
   const elem = video ? 'video autoplay loop muted preload="auto"' : 'img'
   const childHtml = `<${elem} src="${src}"></${elem}>`
-  console.log('childhtml', childHtml)
   frame.html(childHtml)
 }
 
